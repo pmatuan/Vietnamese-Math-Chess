c7562c91b47de0904a6f9903d1a9dcd1960634ba
pmatuan <tuanphanminhanh@gmail.com>
2022-11-30 16:45:50 +0700

    Update move

--- VietnameseMathChess/ChessEngine.py
+++ VietnameseMathChess/ChessEngine.py
@@ -46,6 +46,52 @@ class GameState:
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.redToMove = not self.redToMove  # switch turns back
 
+    '''
+    All moves considering checks
+    '''
+
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    '''
+    All moves without considering checks
+    '''
+
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'r' and self.redToMove) or (turn == 'b' and not self.redToMove):
+                    piece = int(self.board[r][c][1])
+                    self.getPieceMove(r, c, piece, moves)
+        return moves
+
+    def getPieceMove(self, r, c, piece, moves):
+        cross_direction = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        horizon_vertical_direction = ((-1, 0), (0, -1), (1, 0), (0, 1))
+        self.getMoveWithDirection(r, c, piece + 1, cross_direction, moves)
+        self.getMoveWithDirection(r, c, piece + 1, horizon_vertical_direction, moves)
+
+    def getMoveWithDirection(self, r, c, piece, direction, moves):
+        enemyColor = "b" if self.redToMove else "r"
+        for d in direction:
+            for row in range(1, piece):
+                for col in range(1, piece):
+                    endRow = r + d[0] * row
+                    endCol = c + d[1] * col
+                    if 0 <= endRow < 11 and 0 <= endCol < 9:  # on board
+                        endPiece = self.board[endRow][endCol]
+                        if endPiece == "--":  # empty space valid
+                            moves.append(Move((r, c), (endRow, endCol), self.board))
+                        elif endPiece[0] == enemyColor:  # enemy piece valid
+                            moves.append(Move((r, c), (endRow, endCol), self.board))
+                            break
+                        else:
+                            break
+                    else:
+                        break
+
 
 class Move:
     # in chess, fields on the board are described by two symbols, one of them being number between 1-8 (which is
@@ -65,6 +111,16 @@ class Move:
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    '''
+    Overriding the equals method
+    '''
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
 
     def getChessNotation(self):
         # you can add to make this like real chess notation
--- VietnameseMathChess/ChessMain.py
+++ VietnameseMathChess/ChessMain.py
@@ -36,6 +36,8 @@ def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False  # flag variable for when a move is made
     loadImages()
     running = True
     sqSelected = ()  # no square is selected, keep track of the last click of the user (tuple: (row, col))
@@ -56,12 +58,18 @@ def main():
                     playerClicks.append(sqSelected)  # append for both 1st and 2nd clicks
                 if len(playerClicks) == 2:  # after 2 click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()  # reset user clicks
                     playerClicks = []
             elif event.type == p.KEYDOWN:
                 if event.key == p.K_z:  # undo when 'z' is pressed
                     gs.undoMove()
+                    moveMade = True
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
