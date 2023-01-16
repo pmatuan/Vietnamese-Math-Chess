3868180a9a0957f68a957ebdbf0369a7895656ce
pmatuan <tuanphanminhanh@gmail.com>
2023-01-16 14:52:58 +0700

    Update

--- AI/Greedy.py
+++ AI/Greedy.py
@@ -8,7 +8,6 @@ class Greedy(AI):
     def findMove(self, gs, valid_moves):
         turn_multiplier = 1 if gs.red_to_move else -1
         max_score = -self.CHECKMATE
-        best_move = None
         random.shuffle(valid_moves)
         for player_move in valid_moves:
             gs.makeMove(player_move)
@@ -16,5 +15,5 @@ class Greedy(AI):
             gs.undoMove()
             if score > max_score:
                 max_score = score
-                best_move = player_move
-        return best_move
+                self.next_move = player_move
+        return self.next_move
--- AI/Negamax.py
+++ AI/Negamax.py
@@ -7,23 +7,24 @@ class Negamax(AI):
 
     def findMove(self, gs, valid_moves):
         random.shuffle(valid_moves)
-        self.findMoveNegaMaxAlphaBeta(gs, valid_moves, self.DEPTH, -self.CHECKMATE, self.CHECKMATE, 1 if gs.red_to_move else -1)
+        self.findMoveNegaMaxAlphaBeta(gs, valid_moves, self.DEPTH, -self.CHECKMATE, self.CHECKMATE,
+                                      1 if gs.red_to_move else -1)
         return self.next_move
 
     def findMoveNegaMaxAlphaBeta(self, gs, valid_moves, depth, alpha, beta, turn):
         if depth == 0:
             return turn * self.scoreMaterial(gs.board)
-        bestValue = -self.CHECKMATE
+        max_score = -self.CHECKMATE
         for move in valid_moves:
             gs.makeMove(move)
             next_moves = gs.getAllPossibleMoves()
             score = - self.findMoveNegaMaxAlphaBeta(gs, next_moves, depth - 1, -beta, -alpha, -turn)
             gs.undoMove()
-            if score > bestValue:
-                bestValue = score
+            if score > max_score:
+                max_score = score
                 if depth == self.DEPTH:
                     self.next_move = move
-            alpha = max(alpha, bestValue)
+            alpha = max(alpha, max_score)
             if alpha >= beta:
                 break
-        return bestValue
+        return max_score
