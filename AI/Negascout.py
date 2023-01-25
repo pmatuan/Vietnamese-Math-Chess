31d720dbc478ebd2825ad66e16bfc1d61fb88dfe
pmatuan <tuanphanminhanh@gmail.com>
2023-01-25 20:03:44 +0700

    Update negascout

--- AI/Negascout.py
+++ AI/Negascout.py
@@ -4,61 +4,44 @@ from AI.AI import AI
 
 
 class Negascout(AI):
-
     def findMove(self, gs, valid_moves, depth):
         random.shuffle(valid_moves)
         self.DEPTH = depth
-        self.findMoveNegaScoutIterdeep(gs, valid_moves)
-        return self.next_move
-
-    def findMoveNegaScoutIterdeep(self, gs, valid_moves):
-        bestValue = -self.CHECKMATE
         alpha = -self.CHECKMATE
         beta = self.CHECKMATE
-        turn = 1 if gs.red_to_move else -1
-        bestMove = None
-        for d in range(1, self.DEPTH + 1):
-            for move in valid_moves:
-                gs.makeMove(move)
-                next_moves = gs.getAllPossibleMoves()
-                score = -self.quiescenceSearch(gs, -beta, -alpha, -turn, d)
-                gs.undoMove()
-                if score > bestValue:
-                    bestValue = score
-                    bestMove = move
-                if score >= beta:
-                    break
-                alpha = max(alpha, score)
-            if bestMove is not None:
-                self.next_move = bestMove
-            depth = self.getDynamicDepth(gs, bestValue)
-            if depth == 0:
-                break
+        self.next_move = valid_moves[0]
+        self.findMoveNegascout(gs, valid_moves, self.DEPTH, alpha, beta)
+        return self.next_move
 
-    def quiescenceSearch(self, gs, alpha, beta, turn, depth):
+    def findMoveNegascout(self, gs, valid_moves, depth, alpha, beta):
         if depth == 0:
-            return turn * self.scoreMaterial(gs)
-        score = turn * self.scoreMaterial(gs)
-        if score >= beta:
-            return beta
-        if score > alpha:
-            alpha = score
+            return self.quiescenceSearch(gs, alpha, beta)
+        b = beta
+        for i, move in enumerate(valid_moves):
+            gs.makeMove(move)
+            next_moves = gs.getValidMoves()
+            t = -self.findMoveNegascout(gs, next_moves, depth - 1, -b, -alpha)
+            gs.undoMove()
+            if (t > alpha) and (t < beta) and (i > 0):
+                t = -self.findMoveNegascout(gs, next_moves, depth - 1, -beta, -alpha)
+            alpha = max(alpha, t)
+            if alpha >= beta:
+                break
+            b = alpha + 1
+            if depth == self.DEPTH:
+                if alpha > self.scoreMaterial(gs):
+                    self.next_move = move
+        return alpha
+
+    def quiescenceSearch(self, gs, alpha, beta):
+        best_score = self.scoreMaterial(gs)
         captures = gs.getAllPossibleAttacks()
         for move in captures:
             gs.makeMove(move)
-            score = -self.quiescenceSearch(gs, -beta, -alpha, -turn, depth - 1)
+            score = self.quiescenceSearch(gs, alpha, beta)
             gs.undoMove()
-            if score >= beta:
-                return beta
-            if score > alpha:
-                alpha = score
-            beta = alpha + 1
-        return alpha
-
-    def getDynamicDepth(self, gs, score):
-        if score > self.CHECKMATE:
-            return self.DEPTH + 2
-        elif score < -self.CHECKMATE:
-            return 0
-        else:
-            return self.DEPTH
+            best_score = max(best_score, score)
+            alpha = max(alpha, best_score)
+            if alpha >= beta:
+                break
+        return best_score
