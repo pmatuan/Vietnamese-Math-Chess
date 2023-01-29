4cb38b5ea1473358ecca57592d6a492d3967df2d
KayTeeOD <101494254+ktcshust@users.noreply.github.com>
2023-01-29 10:28:46 +0700

    Update

--- main.py
+++ main.py
@@ -267,6 +267,8 @@ def main(gs):
                 running = False
             score_thread = threading.Thread(target=CalTime(gs, screen))
             score_thread.start()
+            score_thread.join()
+            time.sleep(1)
             # AI move finder
             if not game_over and not human_turn and not gs.red_to_move:
                 ################################
